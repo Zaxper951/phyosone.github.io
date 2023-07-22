363d9b5fc40e27ba2d55db17000cc08f0d12639a
Zaxper95 <126374268+Zaxper951@users.noreply.github.com>
2023-07-22 17:02:48 -0500

    the light/dark mode works but still WIP

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let darkModeButton = document.getElementById('dark-mode');
 let lightModeButton = document.getElementById('light-mode');
+let body = document.body;
+let heading1 = document.querySelector('h1');
+let heading2 = document.querySelector('h2');
+let heading3 = document.querySelector('h3');
 
 let enableDarkMode = function(){
     darkModeButton.style.display='none';
     lightModeButton.style.display='inline-block';
+    body.style.backgroundColor='black';
+    heading1.style.color='white';
+    heading2.style.color='white';
+    heading3.style.color='white';
 }
 
 let enableLightMode = function(){
     darkModeButton.style.display='inline-block';
     lightModeButton.style.display='none';
+    body.style.backgroundColor='white';
+    heading1.style.color='#141c3a';
+    heading2.style.color='#141c3a';
+    heading3.style.color='#141c3a';
 }
+
 darkModeButton.addEventListener('click',enableDarkMode);
 lightModeButton.addEventListener('click',enableLightMode);
